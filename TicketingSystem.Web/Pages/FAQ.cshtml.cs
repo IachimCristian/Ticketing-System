b7ff13c2880ede7a4d47ee4ccd966abe33006da8
Iachim Cristian <2238224@student.uma.pt>
2025-05-25 06:21:19 +0100

    Added Night Theme (Work In Progress)

---
+++ TicketingSystem.Web/Pages/FAQ.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TicketingSystem.Web.Pages
+{
+    public class FAQModel : PageModel
+    {
+        public void OnGet()
+        {
+            // FAQ page doesn't need any special logic
+            // All content is static HTML
+        }
+    }
+} 
